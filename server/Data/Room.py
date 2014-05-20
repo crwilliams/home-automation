60af436b9bb35d72dcc9e7ca89b8d0206268517e
Colin Williams <github@crwilliams.co.uk>
2014-05-20 23:25:35 +0100

    Allow values of 'true' (On) and 'false' (Off) for binary switches.  Added is_off function.

--- server/Data/Room.py
+++ server/Data/Room.py
@@ -10,6 +10,15 @@ class Room(object):
         return str(self._value)
 
     def update(self, timestamp, value):
+        if value.lower() == 'true':
+            self._value = True
+        elif value.lower() == 'false':
+            self._value = False
+        else:
+            try:
+                self._value = int(value)
+            except (TypeError, ValueError):
+                self._value = None
         self._value = value
         self._time = timestamp
 
@@ -20,10 +29,14 @@ class Room(object):
         return self._time
 
     def is_on(self):
-        try:
-            return int(self.get_value()) > 0
-        except (TypeError, ValueError):
-            return False
+        return (
+            (type(self.get_value()) == bool and self.get_value() is True)
+            or
+            (type(self.get_value()) == int and self.get_value() > 0)
+        )
+
+    def is_off(self):
+        return not self.is_on()
 
     def get_duration(self):
         return int(math.floor((time.time() - self.get_time()) / 60.0))
