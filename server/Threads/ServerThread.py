cd71fb32400b33d349db43ad07d62076917447d6
Colin Williams <github@crwilliams.co.uk>
2014-05-20 23:21:42 +0100

    Added HTML Photo Page.

--- server/Threads/ServerThread.py
+++ server/Threads/ServerThread.py
@@ -2,6 +2,8 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from xml.sax.saxutils import XMLGenerator
 import json
 import threading
+import os
+import random
 
 from constants import Constants
 from Data.Room import Room
@@ -32,6 +34,16 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 self.output_main_page()
+            elif path_parts[0] == 'photo':
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.output_photo_page()
+            elif path_parts[0] == 'photo.jpg':
+                self.send_response(200)
+                self.send_header('Content-type', 'image/jpeg')
+                self.end_headers()
+                self.output_random_photo('/home/pi/photoframe/')
             elif path_parts[0] == 'data':
                 self.send_response(200)
                 self.send_header('Content-type', 'application/json')
@@ -54,11 +66,26 @@ class MyHandler(BaseHTTPRequestHandler):
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
+    def output_random_photo(self, path):
+        filename = random.choice(os.listdir(path))
+        with open(path + filename, 'r') as f:
+            self.wfile.write(f.read())
+
     def output_main_page(self):
         gen = XMLGenerator(self.wfile, 'utf-8')
         gen.startDocument()
         gen.startElement('html', {'lang': 'en'})
         htmlgen = HtmlPageGenerator(gen)
+        htmlgen.output_head()
+        htmlgen.output_body()
+        gen.endElement('html')
+        gen.endDocument()
+
+    def output_photo_page(self):
+        gen = XMLGenerator(self.wfile, 'utf-8')
+        gen.startDocument()
+        gen.startElement('html', {'lang': 'en'})
+        htmlgen = HtmlPhotoPageGenerator(gen)
         htmlgen.output_head()
         htmlgen.output_body()
         gen.endElement('html')
@@ -166,6 +193,225 @@ $(document).ready( function() {
         }, 'json');
     }, 1000);
 });""")
+        self.gen.endElement('script')
+
+
+class HtmlPhotoPageGenerator(object):
+    def __init__(self, xml_generator):
+        self.gen = xml_generator
+
+    def output_head(self):
+        self.gen.startElement('head', {})
+        self.output_title(Constants.web_page_title)
+        self.output_scripts()
+        self.output_stylesheet()
+        self.gen.endElement('head')
+
+    def output_body(self):
+        self.gen.startElement('body', {
+            'onload': 'startup()',
+            'style': 'text-align: center',
+        })
+        self.gen.startElement('img', {
+            'id': 'photo',
+            'src': '/photo.jpg',
+            'onclick': 'openForm()',
+            'style': 'height: 100%;',
+        })
+        self.gen.endElement('img')
+        self.gen.startElement('form', {})
+        self.gen.startElement('table', {
+            'id': 'lights',
+            'style': 'position: absolute; top: 0px; left: 0px;',
+        })
+
+        for room, room_config in Constants.config.iteritems():
+            try:
+                self.output_row(
+                    room, Constants.values[room_config[2]])
+            except KeyError:
+                pass
+
+        self.gen.startElement('tr', {})
+        self.gen.startElement('td', {
+            'colspan': '3',
+        })
+        self.gen.startElement('input', {
+            'type': 'button',
+            'value': 'close',
+            'onclick': 'closeForm()',
+            'style': 'width: 100%;',
+        })
+        self.gen.endElement('input')
+        self.gen.endElement('td')
+        self.gen.endElement('tr')
+        self.gen.endElement('table')
+        self.gen.endElement('form')
+
+        self.gen.endElement('body')
+
+    def output_title(self, title):
+        self.gen.startElement('title', {})
+        self.gen.characters(title)
+        self.gen.endElement('title')
+
+    def output_stylesheet(self):
+        self.gen.startElement('style', {})
+        self.gen.characters("""
+body {
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+}
+table {
+    font-family: Arial;
+    font-size: 1.5em;
+    padding: 0.5em;
+    margin: 0;
+    background-color: black;
+    color: white;
+    opacity: 0.8;
+}
+input {
+    font-size: 1em;
+    color: gray;
+    background-color: black;
+    height: 100%;
+    width: 3em;
+}
+body, table {
+    width: 100%;
+    height: 100%;
+}
+img {
+    margin: 0;
+    padding: 0;
+}
+""")
+        self.gen.endElement('style')
+
+    def output_row(self, room, actions):
+        action_map = {
+            'on': 'on',
+            'off': 'off',
+            '30': 'l',
+            '60': 'm',
+            '90': 'h',
+        }
+        if len(actions):
+            self.gen.startElement('tr', {})
+            self.gen.startElement('td', {})
+            self.gen.characters(room[0].upper() + room[1:].lower())
+            self.gen.endElement('td')
+            self.gen.startElement('td', {
+                'style': 'text-align: right;',
+            })
+            for action in actions:
+                if action_map.has_key(action):
+                    self.output_button(room, action_map[action], action)
+            self.gen.endElement('td')
+            self.gen.endElement('tr')
+
+    def output_button(self, room, action_name, action):
+        self.gen.startElement('input', {
+            'type': 'button',
+            'id': '%s-%s' % (room, action_name),
+            'value': action_name.upper(),
+            'onclick': 'set("%s", "%s")' % (room, action)})
+        self.gen.endElement('input')
+
+    def output_scripts(self):
+        self.gen.startElement('script', {})
+        self.gen.characters("""
+function httpGet(theUrl, callback, postprocess) {
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    if (callback !== undefined) {
+        xmlHttp.onreadystatechange = alertContents;
+    }
+    xmlHttp.open("GET", theUrl);
+    xmlHttp.send();
+
+    function alertContents() {
+        if (xmlHttp.readyState === 4) {
+            if (xmlHttp.status === 200) {
+                callback(xmlHttp.responseText, postprocess);
+            }
+        }
+    }
+};
+
+function set(room, mode)
+{
+    httpGet('/lights/' + room + '/' + mode);
+}
+
+function openForm()
+{
+    checkStatus(actuallyOpenForm);
+}
+
+function actuallyOpenForm()
+{
+    document.getElementById('photo').style.zIndex = '1';
+    document.getElementById('lights').style.zIndex = '2';
+}
+
+function closeForm()
+{
+    document.getElementById('lights').style.zIndex = '1';
+    document.getElementById('photo').style.zIndex = '2';
+}
+
+function updatePhoto()
+{
+    document.getElementById('photo').src = '/photo.jpg/' + new Date().getTime();
+}
+
+function startup()
+{
+    closeForm();
+    setInterval(updatePhoto, 5000);
+}
+
+function checkStatus(postprocess)
+{
+    httpGet('/data', process, postprocess);
+
+    function process(response, postprocess) {
+        var json = JSON.parse(response);
+        for(var i in json['rooms']) {
+            var onButton = document.getElementById(i + '-on');
+            var offButton = document.getElementById(i + '-off');
+            if (json['rooms'][i] == '0' || json['rooms'][i] == 'False') {
+                if (onButton != null) {
+                    onButton.style.color = 'gray';
+                    onButton.style.backgroundColor = 'black';
+                }
+                if (offButton != null) {
+                    offButton.style.color = 'white';
+                    offButton.style.backgroundColor = 'darkred';
+                }
+            } else if (json['rooms'][i] != null) {
+                if (onButton != null) {
+                    onButton.style.color = 'white';
+                    onButton.style.backgroundColor = 'darkgreen';
+                }
+                if (offButton != null) {
+                    offButton.style.color = 'gray';
+                    offButton.style.backgroundColor = 'black';
+                }
+            }
+        }
+        if (postprocess !== undefined) {
+            postprocess();
+        }
+        if (document.getElementById('photo').style.zIndex == '1') {
+            setTimeout(checkStatus, 1000);
+        }
+    }
+}
+""")
         self.gen.endElement('script')
 
 
